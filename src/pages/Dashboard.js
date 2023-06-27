1546a9651778d34e8b126dc65264223943fbcabf
Kamasani Roja <roja.kamasani@avyata.com>
2023-06-27 10:43:11 +0530

    Hmschanges

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,81 +1,106 @@
-import * as React from 'react';
-import { Grid, Typography, Box,Stack } from '@mui/material';
-import { useTheme } from '@mui/material/styles';
-import { makeStyles } from '@mui/styles';
-import LineGraph from '../charts/lineGraph';
-import CssBaseline from '@mui/material/CssBaseline';
-import BarGraph from '../charts/barChart';
-import PieChart from '../charts/pieChart';
+import React from 'react';
+import Chart from 'react-apexcharts';
+import { Grid, Typography } from '@material-ui/core';
 
-const useStyles = makeStyles((theme) => ({
-    root: {
-        display: 'flex',
-        flexDirection: 'column',
-        margin: '20px',
-        minHeight: '80vh'
+const chartHeight = 300;
+const chartWidth = '100%';
+
+const dataLine = {
+    series: [
+        {
+            name: 'Line Graph',
+            data: [12, 19, 3, 5, 2, 3],
+        },
+    ],
+    options: {
+        chart: {
+            type: 'line',
+            height: chartHeight,
+            width: chartWidth,
+            zoom: {
+                enabled: false,
+            },
+            toolbar: {
+                show: false,
+            },
+        },
+        xaxis: {
+            categories: ['January', 'February', 'March', 'April', 'May', 'June'],
+        },
     },
-    main: {
-        marginTop: theme.spacing(8),
-        marginBottom: theme.spacing(0)
-    }
-}));
+};
 
+const dataBar = {
+    series: [
+        {
+            name: 'Bar Chart',
+            data: [12, 19, 3, 5, 2, 3],
+        },
+    ],
+    options: {
+        chart: {
+            type: 'bar',
+            height: chartHeight,
+            width: chartWidth,
+            zoom: {
+                enabled: false,
+            },
+            toolbar: {
+                show: false,
+            },
+        },
+        xaxis: {
+            categories: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        },
+    },
+};
 
+const dataPie = {
+    series: [12, 19, 3, 5, 2, 3],
+    options: {
+        chart: {
+            type: 'pie',
+            height: chartHeight,
+            width: chartWidth,
+            zoom: {
+                enabled: false,
+            },
+            toolbar: {
+                show: false,
+            },
+        },
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+    },
+};
 
-export default function Dashboard() {
-    const classes = useStyles();
-    const theme = useTheme();
-
+const Dashboard = () => {
     return (
-
-        <div className={classes.root}>
-            <CssBaseline />
-            <Grid container direction="row" justifyContent="space-between" alignItems="baseline">
-                <Grid item>
-                    <Box sx={{ display: 'flex', justifyContent: 'space-start', color: theme.palette.primary.main }}>
-                        {/* <ListIcon /> */}
-                        <Typography variant="h6" component="h6" paddingLeft={1}>
-                            Dashboard
-                        </Typography>
-                    </Box>
+        <div>
+            <Typography variant="h6" component="h6" align="left" gutterBottom style={{ marginTop: '20px', marginLeft: '20px' }}>
+                Dashboard
+            </Typography>
+            <Grid container spacing={2}>
+                <Grid item xs={12} sm={6} md={4}>
+                    <Typography variant="h6" align="center" gutterBottom>
+                        Line Graph
+                    </Typography>
+                    <Chart options={dataLine.options} series={dataLine.series} type="line" height={chartHeight} width={chartWidth} />
                 </Grid>
-            </Grid>
-            <Grid container direction="row" justifyContent="space-between" alignItems="baseline">
-                <Grid item xs={12}>
-
-                    <LineGraph
-                        title="Users"
-                        color="primary.main"
-                        chart={{
-                            colors: [theme.palette.info.main, theme.palette.warning.main],
-                            zoom: {
-                                enabled: false,
-                            },
-                            toolbar: {
-                                show: false,
-                            },
-                        }}
-                    />
-
+                <Grid item xs={12} sm={6} md={4}>
+                    <Typography variant="h6" align="center" gutterBottom>
+                        Bar Chart
+                    </Typography>
+                    <Chart options={dataBar.options} series={dataBar.series} type="bar" height={chartHeight} width={chartWidth} />
                 </Grid>
-                <Grid item xs={12}>
-
-                    <BarGraph
-                        title="Users"
-                        color="primary.main"
-                        chart={{
-                            colors: [theme.palette.info.main, theme.palette.warning.main],
-                            zoom: {
-                                enabled: false,
-                            },
-                            toolbar: {
-                                show: false,
-                            },
-                        }}
-                    />
-
+                <Grid item xs={12} sm={6} md={4}>
+                    <Typography variant="h6" align="center" gutterBottom>
+                        Pie Chart
+                    </Typography>
+                    <Chart options={dataPie.options} series={dataPie.series} type="pie" height={chartHeight} width={chartWidth} />
                 </Grid>
             </Grid>
         </div>
     );
-}
+};
+
+export default Dashboard;
